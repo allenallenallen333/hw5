b07df075056d57a61623e31b81b0de336e20ccac
allenallenallen333 <allen3@utexas.edu>
2018-04-23 14:09:31 -0500

    files

---
+++ paxos/Response.java
@@ -0,0 +1,22 @@
+package paxos;
+import java.io.Serializable;
+
+/**
+ * Please fill in the data structure you use to represent the response message for each RMI call.
+ * Hint: You may need a boolean variable to indicate lack of acceptors and also you may need proposal number and value.
+ * Hint: Make it more generic such that you can use it for each RMI call.
+ */
+public class Response implements Serializable {
+    static final long serialVersionUID=2L;
+    // your data here
+    boolean acceptor;
+    int propNum;
+    Object value;
+
+    // Your constructor and methods here
+    public Response(boolean acceptor, int popNum, Object value) {
+    	this.acceptor = acceptor;
+    	this.propNum = propNum;
+    	this.value = value;
+    }
+}
